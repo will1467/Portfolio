c9d28fa9b02c367b30dfb8ddaab67fa0a39b7820
William Wrigley <williamawrigley@gmail.com>
2018-06-29 13:28:31 +1200

    made website responsive

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
-import { Observable } from 'rxjs/Observable';
-import { catchError, map, tap } from 'rxjs/operators';
-import { of } from 'rxjs/observable/of';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
 
 @Component({
   selector: 'app-dashboard',
@@ -23,50 +15,14 @@ export class DashboardComponent implements OnInit {
     Message: ""
   }
 
+  isNotMobile = true;
+
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-  }
-
-   /**
- * Handle Http operation that failed.
- * Let the app continue.
- * @param operation - name of the operation that failed
- * @param result - optional value to return as the observable result
- */
-private handleError<T> (operation = 'operation', result?: T) {
-  return (error: any): Observable<T> => {
-
-    // TODO: send the error to remote logging infrastructure
-    console.error(error); // log to console instead
-
-    // TODO: better job of transforming error for user consumption
-    console.log(`${operation} failed: ${error.message}`);
-
-    // Let the app keep running by returning an empty result.
-    return of(result as T);
-  };
-}
-
-// addHero (hero : Hero) : Observable<Hero> {
-//   return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
-//     tap((hero: Hero) => this.log(`added hero w/ id = ${hero.id}`)),
-//     catchError(this.handleError<Hero>('addHero'))
-//   )
-// }
-
-  onSubmit() : void {
-    this.sendEmail();
-  }
-
-  sendEmail () : void {
-    console.log("sendEmail called")
-    const url = "http://localhost:8888/email";
-    this.http.post(url, this.emailModel, httpOptions).subscribe(
-      res => {
-        const response = res;
+    if(window.screen.width < 700){
+      this.isNotMobile = false;
     }
-    )
   }
 
 }
